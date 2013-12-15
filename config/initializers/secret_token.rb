037cf1ac4765c0cd68b6707afda8898fae73acda
Prateek057 <bagrecha.prateek@gmail.com>
2013-12-15 06:32:05 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mangaworks::Application.config.secret_token = 'eaa2b49f80fd8afba6f5ffde096309b82268477615672423660ad1789f9615a7f291aaae301e57ad7755789c06aa1af19f5b980efc65eabff11dbafadb269556'
